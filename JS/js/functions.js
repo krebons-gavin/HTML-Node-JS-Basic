dddbce8e11d2203ffe95291e2b82c4fe9800beea
krebons-gavin <krebons@me.com>
2016-01-11 00:12:58 +0800

    2016-01-11, Modified by Gavin

---
+++ JS/js/functions.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Gavin on 16/1/10.
+ */
+
+// 不含参数的函数
+function myFunction(){
+    alert("Hello Gavin! 外部JS");
+}
+
+// 含有参数的函数
+function myFunction2(name, job){
+    alert("Welcome " + name + " , 职位: " +job);
+}
+
+// 带有返回值的函数
+function  multipleNum(a, b){
+    return a*b;
+}
