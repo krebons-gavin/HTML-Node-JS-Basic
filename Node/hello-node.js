188d0c9cb9ea50d09ac365234fedf475f789448b
krebons-gavin <krebons@me.com>
2016-05-25 00:35:35 +0800

    2016-05-25, Modified by Gavin

--- Node/hello-node.js
+++ Node/hello-node.js
@@ -1,6 +1,8 @@
 var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' }); 
-    res.end('Hello World\n'); 
-}).listen(1337, "127.0.0.1"); 
+http.createServer(function(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/plain'
+  });
+  res.end('<h1>Hello Gavin.</h1> <p>Today is 2016-05-25.</p>\n');
+}).listen(1337, "127.0.0.1");
 console.log('Server running at http://127.0.0.1:1337/');
